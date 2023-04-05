5b9d2a79e20594e5448ca091fc8febe1dd266aab
Bruno1390 <124314104+Bruno1390@users.noreply.github.com>
2023-04-05 19:29:46 -0300

    Add files via upload

---
+++ 13.js
@@ -0,0 +1,103 @@
+
+var n1 = parseInt (13);
+var n2 = parseFloat (19.2);
+ var soma = n1-n2; 
+ console.log (soma)
+
+ var n1 = parseInt (2);
+var n2 = parseFloat (10.2);
+ var soma = n1-n2; 
+ console.log (soma)
+
+ var n1 = parseInt (3);
+var n2 = parseFloat (1.2);
+ var soma = n1-n2; 
+ console.log (soma)
+
+ var n1 = parseInt (23);
+var n2 = parseFloat (15.2);
+ var soma = n1-n2; 
+ console.log (soma)
+
+ var n1 = parseInt (4);
+var n2 = parseFloat (13.8);
+ var soma = n1-n2; 
+ console.log (soma)
+
+ var n1 = parseInt (10);
+var n2 = parseFloat (14.2);
+ var soma = n1+n2; 
+ console.log (soma)
+
+
+ var n1 = parseInt (6);
+var n2 = parseFloat (1.5);
+ var soma = n1+n2; 
+ console.log (soma)
+
+ var n1 = parseInt (1);
+var n2 = parseFloat (8.2);
+ var soma = n1+n2; 
+ console.log (soma)
+
+ var n1 = parseInt (1);
+var n2 = parseFloat (1.2);
+ var soma = n1+n2; 
+ console.log (soma)
+
+ var n1 = parseInt (3);
+var n2 = parseFloat (9.2);
+ var soma = n1+n2; 
+ console.log (soma)
+
+
+ var n1 = parseInt (23);
+var n2 = parseFloat (17.2);
+ var soma = n1/n2; 
+ console.log (soma)
+
+
+ var n1 = parseInt (3);
+var n2 = parseFloat (10.2);
+ var soma = n1/n2; 
+ console.log (soma)
+
+ var n1 = parseInt (6);
+var n2 = parseFloat (19.2);
+ var soma = n1/n2; 
+ console.log (soma)
+
+ var n1 = parseInt (3);
+var n2 = parseFloat (1.2);
+ var soma = n1/n2; 
+ console.log (soma)
+
+ var n1 = parseInt (1);
+var n2 = parseFloat (9.2);
+ var soma = n1/n2; 
+ console.log (soma)
+
+ var n1 = parseInt (22);
+var n2 = parseFloat (19.2);
+ var soma = n1*n2; 
+ console.log (soma)
+
+ var n1 = parseInt (13);
+var n2 = parseFloat (19.2);
+ var soma = n1*n2; 
+ console.log (soma)
+
+ var n1 = parseInt (13);
+var n2 = parseFloat (19.2);
+ var soma = n1*n2; 
+ console.log (soma)
+
+ var n1 = parseInt (13);
+var n2 = parseFloat (19.2);
+ var soma = n1*n2; 
+ console.log (soma)
+
+ var n1 = parseInt (13);
+var n2 = parseFloat (19.2);
+ var soma = n1*n2; 
+ console.log (soma)
